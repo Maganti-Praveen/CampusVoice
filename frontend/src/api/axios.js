bdae092f48fd13b2430b1964065a702386b31df2
Maganti Praveen Sai <magantipraveensai3@gmail.com>
2025-10-26 14:20:29 +0530

    Change axios base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://cvbackend-tfs0.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
